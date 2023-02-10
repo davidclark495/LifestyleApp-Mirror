c033c2641db1a246ddf5ec14e8d31ad8520f81f0
davidclark495 <davidclark495@yahoo.com>
2023-02-10 12:48:55 -0700

    Custom "UserCreate" fragment with primitive UI
    Known bugs:
    - number pickers init. data is unformatted, i.e. shows up as "150" instead of "150 lbs"
    - fragment_user_create layout is deeply nested, should be re-tooled as a constraint layout
    - layout just doesn't look great

--- app/src/main/java/com/thegoodlife/MainActivity.kt
+++ app/src/main/java/com/thegoodlife/MainActivity.kt
@@ -1,61 +1,33 @@
 package com.thegoodlife
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.thegoodlife.databinding.ActivityMainBinding
+import com.thegoodlife.UserCreateFragment.ReceiveProfileInterface
+import android.widget.TextView
+import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity(), ReceiveProfileInterface {
+//    private var mStringFirstName: String? = null
+//    private var mTvFirstName: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //Get stuff
+//        mTvFirstName = findViewById<View>(R.id.tv_fn_data) as TextView
 
-        setSupportActionBar(binding.toolbar)
+        //Create the fragment
+//        val submitFragment = SubmitFragment()
+        val userCreateFragment = UserCreateFragment();
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAnchorView(R.id.fab)
-                .setAction("Action", null).show()
-        }
+        //Replace the fragment container
+        val fTrans = supportFragmentManager.beginTransaction()
+        fTrans.replace(R.id.fl_frag_container, userCreateFragment, "userCreat_frag")
+        fTrans.commit()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    override fun receiveUserProfile(data: Array<String?>?) {
+        // do something
     }
 }
---
+++ app/src/main/java/com/thegoodlife/UserCreateFragment.kt
@@ -0,0 +1,113 @@
+package com.thegoodlife
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.NumberPicker
+import android.widget.Spinner
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+
+
+
+/**
+ *
+ */
+class UserCreateFragment : Fragment() {
+
+    private var mNameET: EditText? = null;
+    private var mAgeNumPicker: NumberPicker? = null;
+    private var mWeightNumPicker: NumberPicker? = null;
+    private var mHeightNumPicker: NumberPicker? = null;
+    private var mSexSpinner: Spinner? = null;
+    private var mActivityLevelSpinner: Spinner? = null;
+
+
+    //Callback interface
+    interface ReceiveProfileInterface {
+        fun receiveUserProfile(data: Array<String?>?)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_user_create, container, false)
+
+        // Get stuff
+        mNameET = view.findViewById(R.id.et_name) as EditText;
+        mAgeNumPicker = view.findViewById(R.id.number_age) as NumberPicker;
+        mWeightNumPicker = view.findViewById(R.id.number_weight) as NumberPicker;
+        mHeightNumPicker = view.findViewById(R.id.number_height) as NumberPicker;
+        mSexSpinner = view.findViewById(R.id.spinner_sex) as Spinner;
+        mActivityLevelSpinner = view.findViewById(R.id.spinner_activity_level) as Spinner;
+
+        // Setup stuff
+        // age
+        mAgeNumPicker!!.minValue = 0;
+        mAgeNumPicker!!.maxValue = 150;
+        mAgeNumPicker!!.value = 18;
+        mAgeNumPicker!!.wrapSelectorWheel = false;
+        mAgeNumPicker!!.setOnLongPressUpdateInterval(100);
+        // weight
+        mWeightNumPicker!!.minValue = 0;
+        mWeightNumPicker!!.maxValue = 1000;
+        mWeightNumPicker!!.value = 150;
+        mWeightNumPicker!!.wrapSelectorWheel = false;
+        mWeightNumPicker!!.setOnLongPressUpdateInterval(100);
+        mWeightNumPicker!!.setFormatter(NumberPicker.Formatter { String.format("%d lbs",it) });
+        // height
+        mHeightNumPicker!!.minValue = 0;
+        mHeightNumPicker!!.maxValue = 200;
+        mHeightNumPicker!!.value = 60;
+        mHeightNumPicker!!.wrapSelectorWheel = false;
+        mHeightNumPicker!!.setOnLongPressUpdateInterval(100);
+        mHeightNumPicker!!.setFormatter(NumberPicker.Formatter { String.format("%d' %d\" ",it/12, it%12) });
+
+        // spinner documentation --> https://developer.android.com/develop/ui/views/components/spinner?hl=en
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        // sex (spinner)
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.sex_options,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            mSexSpinner!!.adapter = adapter
+        }
+        // activity level (spinner)
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.activity_level_options,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            mActivityLevelSpinner!!.adapter = adapter
+        }
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Toast.makeText(
+            activity,
+            "Toast! for 'UserCreateFragment.kt'",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+//        _binding = null
+    }
+}
