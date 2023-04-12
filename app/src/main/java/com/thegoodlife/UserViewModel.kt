e7998b6623fdd8ce83dd1e23c85643cf83939bdd
davidclark495 <davidclark495@yahoo.com>
2023-04-11 23:10:30 -0600

    Repositories are connected to ViewModel classes
    - some features/accessors are not yet implemented, see REQs 2-4
    - app is not yet configured to push/pull data to/from ViewModels

--- app/src/main/java/com/thegoodlife/UserViewModel.kt
+++ app/src/main/java/com/thegoodlife/UserViewModel.kt
@@ -4,38 +4,50 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class WeatherViewModel(repository: WeatherRepository) : ViewModel() {
+class UserViewModel(repository: UserRepository) : ViewModel() {
 
     // Connect a live data object to the current bit of weather info
-    private val mWeather: LiveData<WeatherData> = repository.mWeather
+    private val mCurrUser: LiveData<UserData?> = repository.mCurrUser
 
-    private var mWeatherRepository: WeatherRepository = repository
+    private var mUserRepository: UserRepository = repository
 
     //Use a second live data here to show entire contents of db
     // This casts a flow in the repo as a live data so an observer in the view
     // can watch it. If you want to observe variables in the repo from the viewmodel, use
     // observeForever (not recommended as it's almost never needed)
     //
-    // TODO, if time allows (currently weather doesn't have a database)
+    // TODO in REQ4
 //    val allCityWeather: LiveData<List<WeatherFragment>> = repository.allCityWeather.asLiveData()
 
-    fun setLocation(location: String) {
-        // Simply pass the location to the repository
-        mWeatherRepository.updateWeatherAtLocation(location)
+    fun updateCurrUser(updatedUser: UserData?){
+        // TODO
+        mUserRepository.updateCurrUser(updatedUser)
+    }
+
+    fun switchToNewUser(){
+        // TODO in REQ3
+    }
+
+    fun switchToExistingUser(/*take in some ID parameter, whatever is convenient w/ the database*/){
+        // TODO in REQ3
+    }
+
+    fun deleteCurrentUser(){
+        // TODO in REQ3
     }
 
     // Returns the data contained in the live data object
-    val weather: LiveData<WeatherData>
-        get() = mWeather
+    val currUser: LiveData<UserData?>
+        get() = mCurrUser
 }
 
 // This factory class allows us to define custom constructors for the view model
-class WeatherViewModelFactory(private val repository: WeatherRepository) :
+class UserViewModelFactory(private val repository: UserRepository) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(WeatherViewModel::class.java)) {
+        if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return WeatherViewModel(repository) as T
+            return UserViewModel(repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
