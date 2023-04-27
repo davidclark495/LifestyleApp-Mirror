9a40c8a931cbad02238fc10290f327dceefc1437
davidclark495 <davidclark495@yahoo.com>
2023-04-27 11:08:01 -0600

    Simplified UserData class

--- app/src/main/java/com/thegoodlife/UserData.kt
+++ app/src/main/java/com/thegoodlife/UserData.kt
@@ -3,91 +3,34 @@ package com.thegoodlife
 import android.graphics.Bitmap
 import kotlin.math.round
 
-class UserData {
-    private var mName: String? = null
-    private var mAge: Int? = null
-    private var mWeight: Int? = null    // in pounds
-    private var mHeight: Int? = null    // in inches
-    private var mSex: String? = null    // "male" or "female"
-    private var mActivityLevel: String? = null
-    private var mProfilePic: Bitmap? = null
-    private var mProfilePicFilePath: String? = null
-    private var mCountry: String? = null
-    private var mCity: String? = null
-
-    //update for location(?), use in data modification / loading frag
-
-    constructor(
-        name: String?,
-        age: Int?,
-        weight: Int?,
-        height: Int?,
-        sex: String?,
-        activity_level: String?,
-        profile_pic: Bitmap?,
-        profile_pic_file_path: String?,
-        country: String?,
-        city: String?
-    ){
-        mName = name
-        mAge = age
-        mWeight = weight
-        mHeight = height
-        mSex = sex
-        mActivityLevel = activity_level
-        mProfilePic = profile_pic
-        mProfilePicFilePath = profile_pic_file_path
-        mCountry = country
-        mCity = city
-    }
-
-    //Implement properties w/ getters & setters (use member variables as backing fields)
-    var name: String?
-        get() = mName
-        set(value) { mName = value }
-    var age: Int?
-        get() = mAge
-        set(value) { mAge = value }
-    var weight: Int?
-        get() = mWeight
-        set(value) { mWeight = value }
-    var height: Int?
-        get() = mHeight
-        set(value) { mHeight = value }
-    var sex: String?
-        get() = mSex
-        set(value) { mSex = value }
-    var activity_level: String?
-        get() = mActivityLevel
-        set(value) { mActivityLevel = value }
-    var profile_pic: Bitmap?
-        get() = mProfilePic
-        set(value) { mProfilePic = value }
-    var profile_pic_file_path: String?
-        get() = mProfilePicFilePath
-        set(value) { mProfilePicFilePath = value }
-    var country: String?
-        get() = mCountry
-        set(value) { mCountry = value }
+data class UserData (
+    var name: String?,
+    var age: Int?,
+    var weight: Int?,
+    var height: Int?,
+    var sex: String?,
+    var activity_level: String?,
+    var profile_pic: Bitmap?,
+    var profile_pic_file_path: String?,
+    var country: String?,
     var city: String?
-        get() = mCity
-        set(value) { mCity = value }
-    val bmr: Int?
+){
+    val bmr: Int
         get() = calcBMR()
 
     private fun calcBMR(): Int {
-        val kgWeight: Double = mWeight!! * 0.45359237
-        val cmHeight: Double = mHeight!! * 2.54
+        val kgWeight: Double = weight!! * 0.45359237
+        val cmHeight: Double = height!! * 2.54
         var BMRVal: Double
-        if (mSex == "Male") {
-            BMRVal = round(((10 * (kgWeight)) + (6.25 * cmHeight) - (5 * mAge!!) + 5))
+        if (sex == "Male") {
+            BMRVal = round(((10 * (kgWeight)) + (6.25 * cmHeight) - (5 * age!!) + 5))
         }
         else { // same as "else (Female)"
-            BMRVal = round(((10 * (kgWeight)) + (6.25 * cmHeight) - (5 * mAge!!) - 161))
+            BMRVal = round(((10 * (kgWeight)) + (6.25 * cmHeight) - (5 * age!!) - 161))
         }
         // factor in activity level:
-        var activityMultiplier: Double = 1.0
-        when (mActivityLevel) {
+        var activityMultiplier = 1.0
+        when (activity_level) {
             "Extra" ->      activityMultiplier = 1.9
             "Moderate" ->   activityMultiplier = 1.55
             "Light" ->      activityMultiplier = 1.375
